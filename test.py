9c60696da7bcacced7bf08018631ebf076525c86
Boguslav <semenov0310@gmail.com>
2020-11-23 15:59:56 +0200

    Major fixes

--- test.py
+++
@@ -1,9 +0,0 @@
-from microrm.query import Query
-from models.user import User
-from models.user_roles import UserRoles
-
-q = Query(User).select(User.username, User.id).where(User.username == User.id).join(UserRoles, on=(UserRoles.user_id==User.id))
-
-
-print(q.sql)
-print(q._construct_sql())
